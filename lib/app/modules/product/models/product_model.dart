eae6788899af350463c544b6181abce9c6e2788d
Sreejith <csreejithofficial@gmail.com>
2025-06-07 20:58:21 +0530

    first commit

---
+++ lib/app/modules/product/models/product_model.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class ProductModel {
+  final String name;
+  final String price;
+  ProductModel({
+    required this.name,
+    required this.price,
+  });
+  
+}
